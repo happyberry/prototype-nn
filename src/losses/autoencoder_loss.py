0a6e3419b4fb383b221546aa803916695ee849e4
Mentos1105 <jakubraczynski11@wp.pl>
2022-01-03 00:12:18 +0100

    Improved loss functions design

--- src/losses/autoencoder_loss.py
+++ src/losses/autoencoder_loss.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 
 
 class AutoencoderLoss(tf.keras.losses.Loss):
+    def __init__(self, name="autoencoder_loss"):
+        super().__init__(name=name)
+        self.mseLoss = tf.keras.losses.MeanSquaredError()
 
     def call(self, y_true, y_pred):
-        return tf.math.square(tf.norm(y_true - y_pred))
+        return self.mseLoss(y_true, y_pred)
--- src/losses/prototype_network_loss.py
+++ src/losses/prototype_network_loss.py
@@ -1,20 +1,24 @@
 import tensorflow as tf
 
+from src.losses.autoencoder_loss import AutoencoderLoss
+from src.losses.classification_loss import ClassificationLoss
+
 
 class PrototypeNetworkLoss(tf.keras.losses.Loss):
-    def __init__(self, autoencoder_loss_value, r1_term, r2_term, lambda_=0.05, lambda_1=0.05, lambda_2=0.05, name="prototype_network_loss"):
+    def __init__(self, lambda_=0.05, lambda_1=0.05, lambda_2=0.05, name="prototype_network_loss"):
         super().__init__(name=name)
-        self.crossEntropyLoss = tf.keras.losses.CategoricalCrossEntropy(from_logits=False)
-        self.r1 = r1_term
-        self.r2 = r2_term
         self.weights = tf.constant([1.0, lambda_, lambda_1, lambda_2])
         self.lambda_ = lambda_
         self.lambda_1 = lambda_1
         self.lambda_2 = lambda_2
-        self.autoencoder_loss_value = autoencoder_loss_value
+        self.autoencoder_loss = AutoencoderLoss()
         self.classification_loss = ClassificationLoss()
 
+    @tf.function
     def call(self, y_true, y_pred):
-        classification_loss_value = self.classification_loss(y_true, y_pred).item()
-        values = tf.constant([classification_loss_value, self.autoencoder_loss_value, self.r1, self.r2])
+        label, image = y_true
+        predicted_label, reconstruction, r1, r2 = y_pred
+        classification_loss_value = self.classification_loss(label, predicted_label)
+        autoencoder_loss_value = self.autoencoder_loss(image, reconstruction)
+        values = tf.stack([classification_loss_value, autoencoder_loss_value, tf.convert_to_tensor(r1), tf.convert_to_tensor(r2)])
         return tf.tensordot(self.weights, values, axes=1)
