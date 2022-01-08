b29044eec3786b469a517a304729a096e3aebf5d
Mentos1105 <jakubraczynski11@wp.pl>
2022-01-08 23:07:12 +0100

    Added file with image utility functions

---
+++ src/utils/image_utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+
+def display_image(image: np.array, title=""):
+    plt.figure(figsize=(7, 3))
+    if image.shape[-1] == 1:
+        plt.imshow(image, cmap='gray')
+    else:
+        plt.imshow(image)
+    plt.title(title)
+    plt.axis("off")
+    plt.show()
+
+
+def pad_images(images: tf.Tensor, pad_size=2):
+    return tf.pad(images, tf.constant([[0, 0], [pad_size, pad_size], [pad_size, pad_size], [0, 0]]), constant_values=1.)
+
+
+def concatenate_images(images: tf.Tensor):
+    return np.concatenate(images.numpy(), axis=1)
+
+
+def save_image(image: np.array, path: str):
+    plt.imsave(path, image)
